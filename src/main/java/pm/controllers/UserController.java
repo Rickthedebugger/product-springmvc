e08e295889f26832e08bd1162207682b5caa2ea3
Rickthedebugger <riker0951@gmail.com>
2024-11-21 18:17:53 +0530

    sigup and login added

--- src/main/java/pm/controllers/UserController.java
+++ src/main/java/pm/controllers/UserController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MyController {
+public class UserController {
 
 	@RequestMapping("/")
 	public String home() {
 		return "index.jsp";
+	}
+	
+	@RequestMapping("/signup")
+	public String login() {
+		return "signup.jsp";
+	}
+	
+	@RequestMapping("/login")
+	public String signup() {
+		return "login.jsp";
 	}
 }
