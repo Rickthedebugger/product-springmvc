7e108417549b7176e82d52f82bf4a74ccb2ba079
Rickthedebugger <riker0951@gmail.com>
2024-11-21 17:33:50 +0530

    files added

---
+++ src/main/java/pm/config/DispatcherConfig.java
@@ -0,0 +1,25 @@
+package pm.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {MyConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+}
---
+++ src/main/java/pm/config/MyConfiguration.java
@@ -0,0 +1,10 @@
+package pm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("pm")
+@Configuration
+public class MyConfiguration {
+
+}
